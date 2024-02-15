9883c58a61390841d2d387789b017c41eff5ea7e
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2024-02-14 22:16:51 -0600

    Routing functional/button component working

--- labsite/src/components/ClickableButton.tsx
+++ labsite/src/components/ClickableButton.tsx
@@ -1,3 +1,11 @@
 import * as React from "react";
+import {Link} from "react-router-dom";
 
 
+const goToHome = () =>{
+    let path = "/home";
+}
+
+export const ClickableButton = () => {
+    return <button onClick={goToHome}>this is a button</button>
+}
--- labsite/src/views/LandingPage.tsx
+++ labsite/src/views/LandingPage.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
+import {ClickableButton} from "../components/ClickableButton";
 
 export const LandingPage = (): React.ReactElement => {
     console.log("test_LandingPage.tsx");
@@ -9,7 +10,7 @@ export const LandingPage = (): React.ReactElement => {
             <div>
                 Landing Page!!!
                 Click here to go to home page:
-                <Link to={"/home"}>here</Link>
+                <ClickableButton />
             </div>
         </>
     )
